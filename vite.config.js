ae52e826a7bf5b201e0e5fe41c21c03249e3341b
Gridbox <106898591+Gridbox-su@users.noreply.github.com>
2026-09-05 12:23:29 +0300

    Update vite.config.js
    import { defineConfig } from "vite";
    import react from "@vitejs/plugin-react";
    import tailwindcss from "@tailwindcss/vite";
    
    export default defineConfig({
      // ВАЖНО: base должен быть ВНУТРИ объекта
      base: '/palmira/',
      plugins: [react(), tailwindcss()],
      server: {
        host: "0.0.0.0",
        port: 3000,
        strictPort: true,
        hmr: {
          port: 3000,
        },
      },
    });

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-base: '/palmira/'
+
 export default defineConfig({
+  // ВАЖНО: base должен быть ВНУТРИ объекта
+  base: '/palmira/',
   plugins: [react(), tailwindcss()],
   server: {
     host: "0.0.0.0",
